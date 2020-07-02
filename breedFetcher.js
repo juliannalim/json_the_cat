401b4247ccad82917c6b33b07760f5d4d05fb2fa
Julianna <julianna.j.lim@gmail.com>
2020-07-02 17:49:26 -0400

    extracting cat data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,18 @@
-// const findingKitty = function (fileName, callback) {
-//   fs.stat(fileName, (error, stats) => {
-//     if (error) throw error;
-//     callback(findingKitty);
-//   })
-// }
-
 const fetchBreedDescription = function (breedName, callback) {
 
   const request = require('request');
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, function (error, response, body) {
-    //console.error('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log(typeof body); // Print the HTML for the Google homepage.
     const data = JSON.parse(body);
-    //console.log(data);
-    //console.log(typeof data);
-    console.error(("error: " + breedName + " not found!"), error);
-
+    if (data.length === 0) {
+      callback('error', undefined)
+    } else {
+      //console.log(data[0]['description']);
+      //console.log(typeof data);
+      callback(null, data[0]['description'])
+    }
   })
 
 };
-
-// const whenError = function (error) {
-//   console.log("error: " + breedName + " not found!");
-// }
-
 
 
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
+    console.log(error);
     console.log(desc);
   }
 });
